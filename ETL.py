e1050c269ffebd4415109ec9e4e64a4def4ed810
SabrinaPerdiz <sabrinaperdiz.sp@gmail.com>
2023-09-11 20:47:03 -0300

    Adicionado CSV
    Adicionada criação de arquivo csv ao finalizar a transformação, removi as funções e deixei linear, adicionei também comentários para deixar o processo ETL bem visível

--- ETL.py
+++ ETL.py
@@ -4,22 +4,29 @@ from googletrans import Translator
 
 url_base = 'https://favqs.com/api'
 
-def get_quotes(filtro):
-  headers = {'Authorization':'Token token="5bb1866fa53ba1f2147d49e08c338df9"',
-             'Content-Type':'application/json'}
-  response = rq.get(f'{url_base}/quotes/?filter={filtro}&type=tag', headers = headers) 
-  trata_json(response.json())
-
-def trata_json(json):
-  cru = pd.io.json.json_normalize(json)
-  lista = cru.values.tolist()
-  cont = 0
-  while cont < len(lista[0][2]):
-    print(tradutor.translate(lista[0][2][cont]['body'], src='en', dest='pt').text+' - '+lista[0][2][cont]['author'])
-    cont += 1
-
+#Extração
 tradutor = Translator()
-tema = tradutor.translate(input('Insira o tema da frase: '), src='pt', dest='en').text
-get_quotes(tema)
+tema = input('Insira o tema da frase: ') 
+tema_en = tradutor.translate(tema, src='pt', dest='en').text
+headers = {'Authorization':'Token token="5bb1866fa53ba1f2147d49e08c338df9"',
+           'Content-Type':'application/json'}
+response = rq.get(f'{url_base}/quotes/?filter={tema_en}&type=tag', headers = headers) 
+cru = pd.io.json.json_normalize(response.json())
+
+#Transformação
+lista = cru.values.tolist()
+cont = 0
+frases = []
+autores = []
+while cont < len(lista[0][2]):
+  frases.append(tradutor.translate(lista[0][2][cont]['body'], src='en', dest='pt').text)
+  autores.append(lista[0][2][cont]['author'])
+  cont += 1
+dicionario = {'Frases':frases,'Autores':autores}
+df = pd.DataFrame(dicionario)
+
+#Carregamento
+df.to_csv(f'frases_{tema}.csv', sep=';', index=False)
+print('Finalizado!')
 
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# dio-pipeline-etl
+# ETL na tradução de idiomas
+
+### Apresentação
+Em meu projeto utilizando o processo ETL, criei um script para traduzir para o português frases aleatórias dentro de um tema definido pelo usuário. O objetivo é retornar citações creditadas para serem usadas em redações, publicidades, etc.
+
+### Ferramentas
+Para obter as frases de acordo com o tema utilizei a API do site FavQs, que disponibiliza a ferramenta gratuitamente, necessitando somente um cadastro informando o e-mail.
+<br>
+Para realizar a tradução, utilizei a biblioteca googletrans.
+
+>OBS: Por não se tratar de um recurso oficial fornecido pelo Google e sim criada a partir da API oficial, acaba tendo alguns erros em termos de tradução/grafia, no entanto, o resultado é satisfatório e os problemas podem ser relevados por não serem o foco deste projeto.
+
+### Processo ETL
+<b>Extração:</b> feita ao chamar o endpoint https://favqs.com/api/quotes' para obter as frases do tema informado
+
+<b>Transformação:</b> tradução das frases para o português
+
+<b>Carregamento:</b> csv gerado com as frases salvo na máquina
+
+### Links Úteis
+Documentação da API do site FavQs: https://favqs.com/api
+<br>
+Documentação da biblioteca googletrans: https://py-googletrans.readthedocs.io/en/latest/
+<br>
+Documentação da API do Google de tradução: https://cloud.google.com/translate/docs/reference/rest
